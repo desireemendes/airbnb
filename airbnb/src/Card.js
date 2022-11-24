a7d67c80721a34b13c0e8cacf878d3074b05a46b
Desiree Mendes <desireefrost@live.ca>
2022-11-24 18:53:43 -0500

    create card component with hard-coded values

--- airbnb/src/Card.js
+++ airbnb/src/Card.js
@@ -1,6 +1,21 @@
+import card from "./images/katie-zaferes.png";
+import star from "./images/star.png"
 
-function Card () {
+function Card() {
+  return (
+    <section>
+      <img src={card} alt="photo-cards" className="photo-cards" />
+      <div>
+        <p><img src={star} alt="rating" className="rating" /></p>
+        <p>5.0</p>
+        <p>(6)</p>
+      </div>
+      <div>
+        <p>From $136 / person</p>
+      </div>
+    </section>
 
+  );
 }
 
 export default Card;
