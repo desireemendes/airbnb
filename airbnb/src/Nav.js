c85f19bdc2c51c0d000251213412a1759131f8eb
Desiree Mendes <desireefrost@live.ca>
2022-11-24 13:46:12 -0500

    initial commit

---
+++ airbnb/src/Nav.js
@@ -0,0 +1,12 @@
+import logo from './images/airbnb-logo.png'
+import './Nav.css'
+function Nav () {
+    return (
+        <nav>
+
+            <img src={logo} alt="airbnb-logo"/>
+        </nav>
+    )
+}
+
+export default Nav;
